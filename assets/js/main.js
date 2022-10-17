a41c517b07009d77da5602d9c9226a709f39b89f
manuelgaleone <galeonemanuel14@gmail.com>
2022-10-17 14:42:06 +0200

    Working on function

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,27 @@ L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 - Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 */
+
+//Seleziono il bottone dalla DOM
+let startButton = document.getElementById("startButton");
+let container = document.querySelector(".container");
+
+//Creo i quadratini
+let square = `<div class="square"></div>`
+
+startButton.addEventListener("click", function() {
+    
+    squareGenerator();
+    
+});
+
+//Creo la funzione che mi genera i quadratini nella DOM
+function squareGenerator (maxSquare, squarePlace) {
+
+    for (let i = 0; i < maxSquare.length; i++) {
+
+        container.innerHTML += square;
+
+    }
+
+};
