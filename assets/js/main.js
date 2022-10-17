c774a0378614922b32f98d023f010b286d8690fb
manuelgaleone <galeonemanuel14@gmail.com>
2022-10-17 16:51:16 +0200

    Fixed count

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,6 @@ let container = document.querySelector(".container");
 
 //Creo i quadratini
 let maxSquare = 100; 
-let square = `<div id="squareDiv" class="square"></div>`;
 let squareDiv = document.querySelector(".square");
 
 startButton.addEventListener("click", function() {
@@ -26,6 +25,19 @@ startButton.addEventListener("click", function() {
     
 });
 
+//Creo la funzione che mi genera i quadratini nella DOM
+function squareGenerator (maxSquare, squarePlace) {
+    
+    
+    for (let i = 0; i < maxSquare; i++) {
+        
+        let square = `<div id="squareDiv" class="square"><span class="indexNumber">${i}</span></div>`;
+        squarePlace.innerHTML += square;
+
+    }
+
+}
+
 //Elimino i quadratini
 annullaButton.addEventListener("click", function() {
     
@@ -33,37 +45,9 @@ annullaButton.addEventListener("click", function() {
     
 });
 
-//Creo la funzione che mi genera i quadratini nella DOM
-function squareGenerator (maxSquare, squarePlace) {
-
-    for (let i = 0; i < maxSquare; i++) {
-
-        squarePlace.innerHTML += square;
-
-    };
-
-};
-
-//Stampo i calcoli
-let conto = 0;
-
-for (conto = 0; conto < conto.length; i++) {
-
-    numberGenerator(1, 64);
-    console.log(conto);
-};
-
-
 //Creo la funzione di reset DOM
 function squareRemover () {
 
     squareDiv.classList.add("none");
-
-};
-
-//Creo la funzione di calcolo
-function numberGenerator(min, max) {
-
-    return Math.floor(Math.random() * (max - min)) + min;
 
 };
